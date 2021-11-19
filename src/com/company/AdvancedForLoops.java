00ad163f24b8df5014ff8c083f7d95e587792375
student <ishaanpatel@billericak12.com>
2021-11-19 11:28:11 -0500

    November 19, 11:08 AM

---
+++ src/com/company/AdvancedForLoops.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class AdvancedForLoops {
+
+    public static void eights(){
+        int num = 8;
+
+        for (int i = 4; i>0; i--){
+
+            System.out.println(" ");
+
+            for (int y=4; y>i; y--){
+                System.out.print(" ");
+            }
+            for (int z=i; z>0; z--){
+                System.out.print(num);
+                System.out.print(num);
+            }
+            num-=2;
+        }
+    }
+
+    public static void main(String[] args) {
+        eights();
+    }
+}
